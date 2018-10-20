3ef64740dfd33841272657697066c4a06c5f27e5
andinovanprastya <andinovan@gmail.com>
2018-10-20 14:07:00 +0700

    abs

--- app/src/main/java/com/example/aura/koskosan_andi_aura/Kamar.java
+++ app/src/main/java/com/example/aura/koskosan_andi_aura/Kamar.java
@@ -7,18 +7,12 @@ public class Kamar {
     private int gambar;
 
     public static final Kamar[] jeniskamar= {
-            new Kamar("Cilok", "tepung, terasi, air", "1.200.000", R.drawable.loupe),
-            new Kamar("Batagor", "tepung, kacang, daging", "800.0000", R.drawable.home),
-            new Kamar("Bakwan", "tepung, jagung, air", "600.000", R.drawable.home),
-            new Kamar("Cilok", "tepung, terasi, air", "1.200.000", R.drawable.home),
-            new Kamar("Batagor", "tepung, kacang, daging", "800.0000", R.drawable.home),
-            new Kamar("Bakwan", "tepung, jagung, air", "600.000", R.drawable.home),
-            new Kamar("Cilok", "tepung, terasi, air", "1.200.000", R.drawable.home),
-            new Kamar("Batagor", "tepung, kacang, daging", "800.0000", R.drawable.home),
-            new Kamar("Bakwan", "tepung, jagung, air", "600.000", R.drawable.home),
-            new Kamar("Cilok", "tepung, terasi, air", "1.200.000", R.drawable.home),
-            new Kamar("Batagor", "tepung, kacang, daging", "800.0000", R.drawable.home),
-            new Kamar("Bakwan", "tepung, jagung, air", "600.000", R.drawable.home),
+            new Kamar("Compact Room", "Kamar 2x3m, Kasur Kecil, Lemari, Gantungan baju, Kamar mandi bersama, Listrik", "Rp. 500.000 / Bulan", R.drawable.compact),
+            new Kamar("Standart Room", "Kamar 3x4m,Kasur standar, Lemari, Gantungan baju, Kamar mandi bersama, Listrik, Wifi", "Rp. 650.000 / Bulan", R.drawable.standart),
+            new Kamar("Standart Room Doble", "Kamar 3x4m, Kasur standar dobel, Lemari besar, Gantungan baju, Kamar mandi bersama, Listrik, Wifi", "Rp. 750.000 / Bulan", R.drawable.dobel),
+            new Kamar("Premium Room", "Kamar 3x4m, Kasur besar, Lemari besar, Gantungan baju, Kamar mandi pribadi (Shower, Air hangat), Listrik, Wifi, Kipas Angin", "Rp. 1.000.000 / Bulan", R.drawable.premium),
+            new Kamar("Anak Sultan Room", "Kamar 4x7m, Kasur springbed, Lemari Besar, Gantungan baju, Kamar mandi pribadi (shower, bathup, air hangat), Kolam renang pribadi, Kulkas 5 Pintu, AC, TV, Telpon, Dapur pribadi, Pembantu", "Rp. 5.000.000 / Bulan", R.drawable.sultan),
+
     };
 
     public Kamar(String name, String facilitation, String price, int image) {
